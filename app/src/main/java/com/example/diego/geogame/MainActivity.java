fbacb2dc1a4698c95d3ffe5e6bf39d09be27cbc2
Diego Alvarez Ruiz <diegoalvarezruiz1@gmail.com>
2018-01-24 11:04:01 +0100

    Initial commit
    Creando el primer commit.
    Error autentificacion.

---
+++ app/src/main/java/com/example/diego/geogame/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.diego.geogame;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "gpslog";
+    private LocationManager mLocMgr;
+    private TextView textViewGPS, textViewDist;
+
+    private GoogleMap mMap;
+
+    // coordenadas del tesoro
+    private LatLng tesoro;
+    private Location tesoroLoc;
+    private double lat=42.263044, lng=-8.802236;
+
+    //Minimo tiempo para updates en Milisegundos
+    private static final long MIN_CAMBIO_DISTANCIA_PARA_UPDATES = (long) 20; // 20 metro
+    //Minimo tiempo para updates en Milisegundos
+    private static final long MIN_TIEMPO_ENTRE_UPDATES = 10000; // 10 sg
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewGPS = (TextView) findViewById(R.id.lat);
+        textViewDist = (TextView) findViewById(R.id.dist);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        mLocMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //Requiere permisos para Android 6.0
+            Log.e(TAG, "No se tienen permisos necesarios!, se requieren.");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 225);
+            return;
+        } else {
+            Log.i(TAG, "Permisos necesarios OK!.");
+            // registra el listener para obtener actualizaciones
+            mLocMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIEMPO_ENTRE_UPDATES, MIN_CAMBIO_DISTANCIA_PARA_UPDATES, locListener, Looper.getMainLooper());
+        }
+        textViewGPS.setText("Lat " + " Long ");
+        // creamos objetos para determinar el tesoro
+        tesoro = new LatLng(lat,lng);
+        tesoroLoc = new Location(LocationManager.GPS_PROVIDER);
+        tesoroLoc.setLatitude(lat);
+        tesoroLoc.setLongitude(lng);
+    }
+
+    // recibe notificaciones del LocationManager
+    public LocationListener locListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            Log.i(TAG, "Lat " + location.getLatitude() + " Long " + location.getLongitude());
+            textViewGPS.setText("Lat " + (float)location.getLatitude() + " Long " + (float)location.getLongitude());
+
+            // movemos la camara para la nueva posicion
+            LatLng nuevaPosicion = new LatLng(location.getLatitude(),location.getLongitude());
+            CameraPosition cameraPosition = CameraPosition.builder()
+                    .target(nuevaPosicion)
+                    .zoom(15)
+                    .build();
+
+            mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+            //calculamos la distancia a la marca
+            textViewDist.setText(String.valueOf(location.distanceTo(tesoroLoc)));
+        }
+
+        public void onProviderDisabled(String provider) {
+            Log.i(TAG, "onProviderDisabled()");
+        }
+
+        public void onProviderEnabled(String provider) {
+            Log.i(TAG, "onProviderEnabled()");
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.i(TAG, "onStatusChanged()");
+        }
+    };
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //engadimos controls
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        // Engadimos tesoro
+        mMap.addMarker(new MarkerOptions().position(tesoro).title("Marca de Tesoro"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tesoro));
+    }
+}
